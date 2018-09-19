a036733562d17fee0ca2431401d6e62b50490077
Maxime Bolduc <bolduc.max@gmail.com>
2018-09-19 16:30:04 -0400

    Ajout du circuit breaker Retrait des tentatives d'utilisation Eureka non-PCF

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Pivotal.Discovery.Client;
+using Steeltoe.CircuitBreaker.Hystrix;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Steeltoe.Common.Discovery;
 
@@ -33,15 +34,17 @@ namespace AllocationsServer
             services.AddSingleton<IProjectClient>(sp =>
             {
                 var handler = new DiscoveryHttpClientHandler(sp.GetService<IDiscoveryClient>());
-                var httpClient = new HttpClient(handler, false)                    
+                var httpClient = new HttpClient(handler, false)
                 {
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
+                var logger = sp.GetService<ILogger<ProjectClient>>();
+                return new ProjectClient(httpClient, logger);
             });
-            
+
             services.AddDiscoveryClient(Configuration);
+            services.AddHystrixMetricsStream(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +55,8 @@ namespace AllocationsServer
 
             app.UseMvc();
             app.UseDiscoveryClient();
+            app.UseHystrixMetricsStream();
+            app.UseHystrixRequestContext();
         }
     }
 }
---
+++ Components/Timesheets/GetProjectCommand.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Steeltoe.CircuitBreaker.Hystrix;
+using Steeltoe.CircuitBreaker.Hystrix.Strategy.ExecutionHook;
+using Steeltoe.CircuitBreaker.Hystrix.Strategy.Options;
+
+namespace Timesheets
+{
+    public class GetProjectCommand : HystrixCommand<ProjectInfo>
+    {
+        private readonly Func<long, Task<ProjectInfo>> _getProjectFn;
+        private readonly long _projectId;
+        private readonly Func<long, Task<ProjectInfo>> _getProjectFallbackFn;
+
+        public GetProjectCommand(
+            Func<long, Task<ProjectInfo>> getProjectFn,
+            Func<long, Task<ProjectInfo>> getProjectFallbackFn,
+            long projectId
+        ) : base(HystrixCommandGroupKeyDefault.AsKey("ProjectClientGroup"))
+        {
+            _getProjectFn = getProjectFn;
+            _projectId = projectId;
+            _getProjectFallbackFn = getProjectFallbackFn;
+        }
+
+        protected override async Task<ProjectInfo> RunAsync() => await _getProjectFn(_projectId);
+        protected override async Task<ProjectInfo> RunFallbackAsync() => await _getProjectFallbackFn(_projectId);
+    }
+}
